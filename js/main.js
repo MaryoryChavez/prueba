421ad37cffed48a7d372270972801f231916b7c4
MaryoryChavez <maryory.chavez@gmail.com>
2017-03-21 16:56:13 -0500

    navbar 90%

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    console.log("si");
+})
+
+$('.menu-btn').on('click', function () {
+
+    var elem = $(this),
+        item = $('.menu-item'),
+        active = 'is-active',
+        play = 'menu-item-play';
+
+    if (elem.hasClass(active)) {
+        elem.removeClass(active);
+        $(item.get().reverse()).each(function (i) {
+            var row = $(this);
+            setTimeout(function () {
+                row.removeClass(play);
+            }, 50 * i);
+        });
+    } else {
+        elem.addClass(active);
+        item.each(function (i) {
+            var row = $(this);
+            setTimeout(function () {
+                row.addClass(play);
+            }, 50 * i);
+        });
+    }
+
+});
