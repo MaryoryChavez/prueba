07fb0c61d8961ef7bd79800ab844b8d09cabf281
MaryoryChavez <maryory.chavez@gmail.com>
2017-03-22 18:27:30 -0500

    hero section 90%

--- js/main.js
+++ js/main.js
@@ -28,3 +28,30 @@ $('.menu-btn').on('click', function () {
     }
 
 });
+
+
+function maquina(contenedor,texto,intervalo){
+   // Calculamos la longitud del texto
+   var longitud = texto.length;
+   // Obtenemos referencia del div donde se va a alojar el texto.
+   var cnt = document.getElementById(contenedor);
+   var i=0;
+   // Creamos el timer
+   var timer = setInterval(function(){
+      // Vamos añadiendo letra por letra y la _ al final.
+      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
+      // Si hemos llegado al final del texto..
+      if(i >= longitud){
+         // Salimos del Timer y quitamos la barra baja (_)
+         clearInterval(timer);
+         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+         return true;
+      } else {
+         // En caso contrario.. seguimos
+         i++;
+      }},intervalo);
+};
+
+var texto = "hola! mi nombre es maryory chavez. web developer.";
+// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
+maquina("maquinas",texto,100);
