0c30b531f37cd65f064472a9440771d7295901a7
MaryoryChavez <maryory.chavez@gmail.com>
2017-03-27 03:42:15 -0500

    re-edicion

--- js/main.js
+++ js/main.js
@@ -29,29 +29,21 @@ $('.menu-btn').on('click', function () {
 
 });
 
+//----------->> Mensaje Hero <<-----------
 
-function maquina(contenedor,texto,intervalo){
-   // Calculamos la longitud del texto
-   var longitud = texto.length;
-   // Obtenemos referencia del div donde se va a alojar el texto.
-   var cnt = document.getElementById(contenedor);
+function maquina(container,texto,intervalo){
+   var long = texto.length;
+   var cnt = document.getElementById(container);
    var i=0;
-   // Creamos el timer
    var timer = setInterval(function(){
-      // Vamos añadiendo letra por letra y la _ al final.
       cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "_";
-      // Si hemos llegado al final del texto..
-      if(i >= longitud){
-         // Salimos del Timer y quitamos la barra baja (_)
+      if(i >= long){
          clearInterval(timer);
-         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+         cnt.innerHTML = cnt.innerHTML.substr(0,long);
          return true;
       } else {
-         // En caso contrario.. seguimos
          i++;
       }},intervalo);
 };
-
-var texto = "hola! mi nombre es maryory chavez. web developer.";
-// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
-maquina("maquinas",texto,100);
+var texto = "¡hola! mi nombre es maryory chavez. web developer.";
+maquina("letras",texto,110);
